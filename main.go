ee9ec1d3bab1c38272bdfa6b4792e2c282dd3b62
Leandro Pereira Alves <leandro.alves@everymind.com.br>
2021-06-25 12:01:58 -0300

    Release v2.8.5

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ import (
 	"github.com/robfig/cron/v3"
 )
 
-var version = "2.8.4"
+var version = "2.8.5"
 
 func init() {
 	// Setting the limits the number of operating system threads that can execute user-level Go code simultaneously.
